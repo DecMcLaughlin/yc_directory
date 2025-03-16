e44934ab1443e9e1ec0dd1cd7f17ceb723746466
DecMcLaughlin <comptasia@outlook.com>
2025-03-16 09:23:30 +0000

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,47 @@
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const posts = [{
+    _createdAt: new Date(),
+    views:55,
+    author: {_id:1, name:"John Doe"},
+    _id: 1,
+    description: "Description of Startup 1",
+    image: "https://mecaluxcom.cdnwm.com/img/logistics-articles/advancements-humanoid-robots-simulations.1.11.jpg?imwidth=1024",
+    category:"Robots",
+    title: "We Robots",
+  }];
+
+  return (
+    <>
+      <section className="pink_container ">
+        <h1 className="heading">Pitch Your Startup,<br />Connect with Enterpeneurs</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Votes and Pitches, and Get Noticed in Virtual Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: any, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+ 
+         </ul>
+      </section>
+    </>
+  );
+}
